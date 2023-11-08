1a95eb201c5aa8fc61a0c3cdcfbc645214da59c8
Данил <danikkach666@gmail.com>
2023-11-08 22:18:09 +0200

    add

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+  let war = title.toLowerCase();
+  let hammer = war.split(' ');
+  let slug = hammer.join('-');
+  return slug;
+}
+
+console.log(slugify('Arrays for begginers'));
+console.log(slugify('English for developer'));
+console.log(slugify('Ten secrets of JavaScript'));
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
